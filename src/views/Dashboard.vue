da674acff2e7be7bd133354c5fe0c3c7ae992f09
Anup Khandelwal <anup4khandelwal@gmail.com>
2021-03-13 23:15:42 +0530

    Adding Intersection directives

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -29,6 +29,25 @@
       </v-col>
     </v-row>
 
+    <v-row id="below-the-fold" v-intersect="showMoreContent">
+      <v-col cols="12" md="8">
+        <EmployeesTable :employees="employees" @select-employee="setEmployee" />
+      </v-col>
+      <v-col cols="12" md="4">
+        <EventTimeline :timeline="timeline" />
+      </v-col>
+    </v-row>
+
+    <v-row v-if="loadNewContent" id="more-content">
+      <v-col>
+        <v-skeleton-loader
+          ref="skeleton"
+          type="table"
+          class="mx-auto"
+        ></v-skeleton-loader>
+      </v-col>
+    </v-row>
+
     <v-snackbar v-model="snackbar" :left="$vuetify.breakpoint.lgAndUp">
       You have selected {{ selectedEmployee.name }},
       {{ selectedEmployee.title }}
@@ -44,10 +63,12 @@ import EmployeesTable from '../components/EmployeesTable'
 import EventTimeline from '../components/EventTimeline'
 import SalesGraph from '../components/SalesGraph'
 import StatisticCard from '../components/StatisticCard'
+
 import employeesData from '../data/employees.json'
 import timelineData from '../data/timeline.json'
 import salesData from '../data/sales.json'
 import statisticsData from '../data/statistics.json'
+
 export default {
   name: 'DashboardPage',
   components: {
@@ -59,6 +80,7 @@ export default {
   data() {
     return {
       employees: employeesData,
+      loadNewContent: false,
       sales: salesData,
       selectedEmployee: {
         name: '',
@@ -74,6 +96,9 @@ export default {
       this.snackbar = true
       this.selectedEmployee.name = event.name
       this.selectedEmployee.title = event.title
+    },
+    showMoreContent(entries) {
+      this.loadNewContent = entries[0].isIntersecting
     }
   }
 }
